4e9794e44ea7a2adbd4cd1703324d267cac23a1e
Alex Evans <evansalex2000@gmail.com>
2026-02-17 09:40:20 -0500

    Generation 8

--- configuration.nix
+++ configuration.nix
@@ -114,8 +114,8 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # wget
      vivaldi
-    # bitwarden-desktop
-    # obsidian
+     bitwarden-desktop
+     obsidian
      gromit-mpx
      remmina
      git
@@ -129,6 +129,7 @@
      openvpn3
      beammp-launcher      
      asusctl
+     discord
   ];
 
 #test
