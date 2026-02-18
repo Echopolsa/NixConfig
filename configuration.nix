2af175c5a0295ff3bc0d33459ec84f4bf0579dca
Alex Evans <evansalex2000@gmail.com>
2026-02-18 17:49:44 -0500

    Generation 13

--- configuration.nix
+++ configuration.nix
@@ -111,7 +111,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     # wget
      vivaldi
      bitwarden-desktop
      obsidian
